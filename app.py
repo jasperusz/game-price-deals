a52974a4cb41dc8a75696c565bce5d7299937ce5
jasperusz <235980385+jasperusz@users.noreply.github.com>
2026-06-16 21:27:38 -0300

    feat: def search_game now returns game title and id selected by user

--- app.py
+++ app.py
@@ -16,17 +16,27 @@ def search_game(game_name):
         "title": game_name
             }
         )
-        print(response.status_code)
+        print(f'Status Code: {response.status_code}')
+
+        # If the Search find results
         if response.status_code == 200:
             results = response.json()
             for i, result in enumerate(results, start=1):
                 print(f'{i}: {result['title']} ({result['type']})')
+            choice = int(input('Select the game from the list: '))
+            user_selection = results[choice - 1]
+            searched_game_name = user_selection['title']
+            searched_game_id = user_selection['id']
+            return f'You selected {searched_game_name}.\nGame ID: {searched_game_id}'
+            
+        # If the search didn't find the game
         if response.status_code == 404:
             return {
                 "error": "Game not found.",
                 "status_code": response.status_code,
                 "reason": response.reason
             }
+        # If there's data validation error response
         if response.status_code == 400:
             return {
                 "error": "Bad request",
